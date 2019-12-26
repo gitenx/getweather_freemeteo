add4d057995e9f6363c4a84baefe0d78c250ba6d
gitenx <xnorthenx@gmail.com>
2019-12-26 21:18:16 +0700

    update

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 Extract monthly weather history from freemeteo (https://freemeteo.co.id/)
 
-I use beautifulsoup for scrapping and pandas for creating csv file. The data in csv not yet cleaned (in this repo I enclose the csv file). The city is still manually set, in this case I set it to Jakarta.
+I use beautifulsoup for scrapping and pandas for creating csv file. The data in csv not yet cleaned (you can check the csv file). The city is still manually set, in this case I set it to Jakarta.
 
 How to use:
 - git clone https://github.com/gitenx/getweather_freemeteo.git
 - cd getweather_freemeteo
 - pip install -r requirements.txt
 - python get_weather.py
+
+**UPDATE**
+- now can input different city
+- little fix
 
 Enjoy it.
--- get_weather.py
+++ get_weather.py
@@ -7,11 +7,11 @@ import requests
 import pandas as pd
 
 
-def get_weather(beginmonth, endmonth, year):
+def get_weather(city, beginmonth, endmonth, year):
     weather = []
-    for month in range(beginmonth, endmonth):
-        theurl = 'https://freemeteo.co.id/weather/jakarta/history/monthly-history/?gid=1642911&station=26293&month=' + str(
-            month) + '&year=' + str(year) + '&language=english&country=indonesia'
+
+    for month in range(beginmonth, endmonth+1):
+        theurl = 'https://freemeteo.co.id/weather/' + city + '/history/monthly-history/?gid=1642911&station=26293&month=' + str(month) + '&year=' + str(year) + '&language=english'
 
         thepage = requests.get(theurl)
         soup = BeautifulSoup(thepage.text, 'html.parser')
@@ -24,20 +24,53 @@ def get_weather(beginmonth, endmonth, year):
                      temp.find_all('tr')[i].find_all('td')[9].text])
 
     dfweather = pd.DataFrame(weather, columns=['date', 'mintemp', 'maxtemp', 'windspeed', 'weatherevent'])
+    fname = 'weather_{}_{}to{}_{}'.format(city,begin_month, end_month, year)
 
-    dfweather.to_csv('weather.csv', index=False)
+    dfweather.to_csv(fname + '.csv', index=False)
+
+
+def find_city(city):
+    found = True
+    result = ''
+
+    theurl = 'https://freemeteo.co.id/weather/search/?q=' + city + '&pg=0&language=english'
+
+    thepage = requests.get(theurl)
+    soup = BeautifulSoup(thepage.text, 'html.parser')
+
+    for c in soup.find_all('p', attrs={'class': 'title no-results'}):
+        result = c.find_all('span')[0].text
+    
+    if result == 'Sorry, no results':
+        found = False
+    else:
+        found = True
+    
+    return found
 
 
 if __name__ == '__main__':
     print("""
-    To use this: input year of the weather you want to extract (e.g. 2019).
-    input range months (e.g. begin month=1, end month=10 => it means jan to september)
-    
-    For this version the city set to Jakarta.
+    How to use:
+    - input city (e.g. jakarta)
+    - input year of the weather you want to extract (e.g. 2019).
+    - input range months (e.g. begin month=1, end month=10 => it means jan to oct)
     """)
-    year = int(input('Year: '))
-    begin_month = int(input('Begin month: '))
-    end_month = int(input('End month: '))
-    print('Processing...')
-    get_weather(begin_month, end_month, year)
-    print('it finished, check weather.csv in your directory')
+    while True:
+        city =  input('City: ')
+        year = int(input('Year: '))
+        begin_month = int(input('Begin month: '))
+        end_month = int(input('End month: '))
+
+        if find_city(city):
+            if year < 0 or len(str(year)) < 4 or begin_month < 0 or end_month < 0 or begin_month > 12 or end_month > 12 or begin_month > end_month:
+                print('year or begin month or end month might be wrong, please check!\n')
+            else:
+                print('Processing...')
+                get_weather(city, begin_month, end_month, year)
+                print('it finished, check weather_{}_{}to{}_{}.csv in your directory'.format(city,begin_month,end_month,year))
+
+                break
+        else:
+            print('city you entered not found! please check!\n')
+
